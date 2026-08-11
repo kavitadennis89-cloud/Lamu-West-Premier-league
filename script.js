8d6ed3d50ac72cb042f595307ef616faabd011f7
kavitadennis89-cloud <kavitadennis89@gmail.com>
2026-08-11 17:47:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Lamu West Premier League loaded successfully!");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQIYS4TaMNIokWDCn0EJhlaA6KBxCmyaQ",
+  authDomain: "lamu-west-premier-league.firebaseapp.com",
+  projectId: "lamu-west-premier-league",
+  storageBucket: "lamu-west-premier-league.firebasestorage.app",
+  messagingSenderId: "280853181931",
+  appId: "1:280853181931:web:8c411d3528bddadd2d15ae"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+async function loadTeams() {
+  const querySnapshot = await getDocs(collection(db, "teams"));
+
+  querySnapshot.forEach((doc) => {
+    console.log(doc.id, doc.data());
+  });
+}
+
+loadTeams();

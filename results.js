8d8bbd73b63f72577150709cc2ec434745d8d7fe
kavitadennis89-cloud <kavitadennis89@gmail.com>
2026-08-11 22:55:43 +0300

    Update results.js

--- results.js
+++ results.js
@@ -9,13 +9,22 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBQIYS4TaMNIokWDCn0EJhlaA6KBxCmyaQ",
-  authDomain: "lamu-west-premier-league.firebaseapp.com",
-  projectId: "lamu-west-premier-league",
-  storageBucket: "lamu-west-premier-league.firebasestorage.app",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+
+  apiKey: "AIzaSyBQIYS4TaMNIokWDCn0EJhlaA6KBxCmyaQ",
+
+  authDomain: "lamu-west-premier-league.firebaseapp.com",
+
+  projectId: "lamu-west-premier-league",
+
+  storageBucket: "lamu-west-premier-league.firebasestorage.app",
+
+  messagingSenderId: "280853181931",
+
+  appId: "1:280853181931:web:8c411d3528bddadd2d15ae",
+
+  measurementId: "G-HQ04SZWBBB"
 };
+  a
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);

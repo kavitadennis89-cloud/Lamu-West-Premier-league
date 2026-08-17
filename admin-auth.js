92c855096aa3958c0f6b20e0713674c134745a93
kavitadennis89-cloud <kavitadennis89@gmail.com>
2026-08-17 22:10:10 +0300

    Update admin-auth.js

--- admin-auth.js
+++ admin-auth.js
@@ -1,21 +1,9 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
-
 import {
   getAuth,
   onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBQIYS4TaMNIokWDCn0EJhlaA6KBxCmyaQ",
-  authDomain: "lamu-west-premier-league.firebaseapp.com",
-  projectId: "lamu-west-premier-league",
-  storageBucket: "lamu-west-premier-league.firebasestorage.app",
-  messagingSenderId: "280853181931",
-  appId: "1:280853181931:web:8c411d3528bddadd2d15ae"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+const auth = getAuth();
 
 onAuthStateChanged(auth, (user) => {
 
